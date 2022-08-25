52bb5c6a71e953c6531b58a340d8be0dc9b5f8fc
Epep <ipinggxlirangg@gmail.com>
2022-08-25 17:09:31 +0800

    First Commit

---
+++ src/data/feedbackData.js
@@ -0,0 +1,29 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+  },
+  {
+    id: 2,
+    rating: 5,
+    text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+  },
+  {
+    id: 3,
+    rating: 9,
+    text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+  },
+  {
+    id: 4,
+    rating: 10,
+    text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+  },
+  {
+    id: 5,
+    rating: 8,
+    text: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt',
+  }
+]
+
+export default FeedbackData;
