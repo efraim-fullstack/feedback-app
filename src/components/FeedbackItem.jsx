8168ed63e90f9517fc9857b6e390ef0a99be2530
Epep <ipinggxlirangg@gmail.com>
2022-08-30 15:07:59 +0800

    Edit Added

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,20 @@
-import { FaTrash } from 'react-icons/fa'
+import { FaTimes, FaEdit } from 'react-icons/fa'
+import { useContext } from 'react'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
+import FeedbackContext from '../context/FeedbackContext'
 
-function FeedbackItem({item, handleDelete}) {
+function FeedbackItem({ item }) {
+  const {deleteFeedback, editFeedback} = useContext(FeedbackContext)
 
   return (
   <Card>
     <div className="num-display">{item.rating}</div>
-    <button onClick={() => handleDelete(item.id)} className="close">
-      <FaTrash color='purple' />
+    <button onClick={() => deleteFeedback(item.id)} className="close">
+      <FaTimes color='purple' />
+    </button>
+    <button className="edit">
+      <FaEdit onClick={() => editFeedback(item)} color='purple' />
     </button>
     <div className="text-display">{item.text}</div>
   </Card>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,31 +1,14 @@
-import { v4 as uuidv4 } from 'uuid'
-import { useState } from "react"
 import FeedbackList from '../components/FeedbackList'
-import FeedbackData from "../data/feedbackData"
 import FeedbackStats from "../components/FeedbackStats"
 import FeedbackForm from "../components/FeedbackForm"
 import AboutIconLink from '../components/AboutIconLink'
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete Feedback?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-    
-  }
-
   return (
     <>
-      <FeedbackForm handleAdd={addFeedback} />
-      <FeedbackStats feedback={feedback} />
-      <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      <FeedbackForm />
+      <FeedbackStats />
+      <FeedbackList />
       <AboutIconLink />
     </>
   )
